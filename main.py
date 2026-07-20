daf3b9e4dd8f03a96a8b2fb81e3e6bf0f5b05b62
EuphoriaBot <dimasfirja80@gmail.com>
2026-07-20 12:18:28 +0800

    Background

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 import configs
+import assets
+from objects.background import Background
 
 pygame.init()
 
@@ -7,12 +9,22 @@ screen = pygame.display.set_mode((configs.SCREEN_WIDTH, configs.SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 running = True
 
+assets.load_sprite()
+
+sprite = pygame.sprite.LayeredUpdates()
+
+Background(0, sprite)
+Background(1, sprite)
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     screen.fill("pink")
+
+    sprite.draw(screen)
+    sprite.update()
 
     pygame.display.flip()
     clock.tick(configs.FPS)
