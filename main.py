46a94aedfecad1d6b5f32fe1e8d69bf24f50d529
EuphoriaBot <dimasfirja80@gmail.com>
2026-07-22 15:46:04 +0800

    Column Sprite

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import configs
 import assets
 from objects.background import Background
 from objects.floor import Floor
+from objects.column import Column
 
 pygame.init()
 
 screen = pygame.display.set_mode((configs.SCREEN_WIDTH, configs.SCREEN_HEIGHT))
 clock = pygame.time.Clock()
+column_create_event = pygame.USEREVENT
 running = True
 
 assets.load_sprites()
@@ -16,14 +18,20 @@ sprite = pygame.sprite.LayeredUpdates()
 
 Background(0, sprite)
 Background(1, sprite)
-
 Floor(0, sprite)
 Floor(1, sprite)
+
+Column(sprite)
+
+pygame.time.set_timer(column_create_event, millis=1500)
+
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == column_create_event:
+            Column(sprite)
 
     screen.fill("pink")
 
